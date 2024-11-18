a62e7aa7cf63effc24b5cd5a24b70aaed61d654c
Tobias Töx <toex.tobias@gmail.com>
2024-11-18 21:24:16 +0100

    Navigator storage

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ if ("serviceWorker" in navigator) {
       });
   });
 }
+
+navigator.storage.persist().then(function (persistent) {
+  if (persistent)
+    console.log("Storage will not be cleared except by explicit user action");
+  else console.log("Storage may be cleared by the UA under storage pressure.");
+});
